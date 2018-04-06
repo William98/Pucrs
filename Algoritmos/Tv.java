a14b2a06cfe76935100e2eadd2501f0ec6384125
Lucas Ribeiro <33958806+lucasmribeiro@users.noreply.github.com>
2018-04-05 22:58:13 -0300

    commit

--- Algoritmos/Tv.java
+++ Algoritmos/Tv.java
@@ -1,8 +1,31 @@
+package televisao;
+
 public class Tv{
+
+  private class Canal{
+
+    private int numeroCanal;
+    private float frequencia[2];
+    private Canal next;
+
+    public Canal(int numeroCanal, float frequenciaX, float frequenciaY, Canal next){
+        this.numeroCanal = numeroCanal;
+        this.frequencia[0] = frequenciaX;
+        this.frequencia[1] = frequenciaY;
+        this.next = next;
+    }
+
+    public int getNome(){ return numeroCanal; }
+    public float[] getFrequencia(){ return frequencia; }
+    public Canal getNext(){ return next; }
+    public void setNext(Canal c){ next = c;}
+
+  }
 
   private boolean on;
   private boolean mute;
-  private int numeroCanal; /* @TODO criar classe canal */
+  private Canal canal; /* @TODO criar classe canal */
+  private int qtdCanais;
   private int volume;
 
   /* Construtores */
@@ -10,12 +33,12 @@ public class Tv{
     on = false;
     mute = false;
     volume = 0;
-    numeroCanal = 0; //
+    qtdCanais = 0;
   }
-  public Tv(boolean on, boolean mute, int canal, int volume){
+  public Tv(boolean on, boolean mute, Canal canal, int volume){
     this.on = on;
     this.mute = mute;
-    this.numeroCanal = canal;
+    this.canal = canal;
     this.volume = volume;
   }
 
@@ -52,6 +75,13 @@ public class Tv{
     else{
       System.out.println("Volume está no mínimo");
     }
+  }
+
+  /* @TODO Metodo storeCanal */
+  public void storeCanal(int numero, float x, float y){
+    canal = new Canal(numero, x, y, null);
+    canal.setNext();
+    qtdCanais++;
   }
 
   /* @TODO Metodo para aumentar Canal 0 até 100 */
