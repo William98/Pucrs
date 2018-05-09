f8206b7fd94c0cb0817079bbb51dcf5f3d4c90ee
Lucas Ribeiro <33958806+lucasmribeiro@users.noreply.github.com>
2018-05-08 21:24:28 -0300

    Update SortedList.java

--- Algoritmos/Exercicios/SortedList.java
+++ Algoritmos/Exercicios/SortedList.java
@@ -5,16 +5,16 @@
 
 public class DoublyLinkedList{
   private class Node {
-    private Entry entry;
+    private String data;
     private Node prev;
     private Node next;
 
-    public Node(Entry e, Node p, Node n) {
-        entry = e;
+    public Node(String d, Node p, Node n) {
+        data = d;
         prev = p;
         next = n;
     }
-    public Entry getElement() { return entry; }
+    public String getElement() { return data; }
     public Node getPrev() { return prev; }
     public Node getNext() { return next; }
     public void setPrev(Node p) { prev = p; }
@@ -32,60 +32,109 @@ public class DoublyLinkedList{
   }
   public int size() { return size; }
   public boolean isEmpty() { return size == 0; }
-  public Entry first() {
+  public String first() {
       if (isEmpty()) return null;
       return header.getNext().getElement();
   }
-  public Entry last() {
+  public String last() {
       if (isEmpty()) return null;
       return trailer.getPrev().getElement();
   }
-  public void addFirst(Entry e) {
-      addBetween(e, header, header.getNext());
+  public void addFirst(String d) {
+      addBetween(d, header, header.getNext());
   }
-  public void addLast(Entry e) {
-      addBetween(e, trailer.getPrev(), trailer);
+  public void addLast(String d) {
+      addBetween(d, trailer.getPrev(), trailer);
   }
-  /*public Entry removeFirst() {
-      if (isEmpty()) return null;
-      return remove(header.getNext());
-  }*/
-  // public Entry removeLast() {}
-  public void addBetween(Entry e, Node predecessor, Node successor) {
-      Node newest = new Node(e, predecessor, successor);
+  public String removeFirst(){
+    if(isEmpty()) return null;
+    Node aux = header;
+    header = header.getNext();
+    size--;
+    return aux.getElement();
+
+  }
+  public String removeLast(){
+    if(isEmpty()) return null;
+    Node aux = trailer;
+    trailer = trailer.getPrev();
+    size--;
+    return aux.getElement();
+  }
+  public void addBetween(String d, Node predecessor, Node successor) {
+      Node newest = new Node(d, predecessor, successor);
       predecessor.setNext(newest);
       successor.setPrev(newest);
       size++;
   }
-  /* public Entry remove(Node node) {
-      ...
-      size--;
-      return node.getElement;
-  }*/
+// @TODO testar metodo Sort e Ajustar metodos de exclusão para padrão abaixo
+  public void Sort(){
+    Node aux1, aux2;
+  }
+  public void QuickSort(){
 
+  }
+// public Entry removeFirst() {
+//    if (isEmpty()) return null;
+//    return remove(header.getNext());
+//  }
+// public Entry removeLast() {}
+// public String remove(Node n) {
+//
+//     size--;
+//     return n.getElement();
+// }
 }
 
-/*
+/* @TODO
  * 2 - Desenvolver a classe SortedList
  *
  */
 
  class SortedList{
    private class Node {
-     private Entry entry;
+     private String data;
      private Node prev;
      private Node next;
 
-     public Node(Entry e, Node p, Node n) {
-         entry = e;
+     public Node(String d, Node p, Node n) {
+         data = d;
          prev = p;
          next = n;
      }
-     public Entry getElement() { return entry; }
+     public String getElement() { return data; }
      public Node getPrev() { return prev; }
      public Node getNext() { return next; }
      public void setPrev(Node p) { prev = p; }
      public void setNext(Node n) { next = n; }
+   }
+
+   private Node header;
+   private Node trailer;
+   private int size = 0;
+
+   public int size() { return size; }
+   public boolean isEmpty() { return size == 0; }
+   public String first() {
+       if (isEmpty()) return null;
+       return header.getNext().getElement();
+   }
+   public String last() {
+       if (isEmpty()) return null;
+       return trailer.getPrev().getElement();
+   }
+   public void addElement(String d){
+     if(isEmpty())
+      addBetween(d, header, header.getNext());
+    else{
+      
+    }
+   }
+   public void addBetween(String d, Node predecessor, Node successor) {
+       Node newest = new Node(d, predecessor, successor);
+       predecessor.setNext(newest);
+       successor.setPrev(newest);
+       size++;
    }
 
 
